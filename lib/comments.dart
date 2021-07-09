9b58eb8824c5eaf4c33b781e59b460b9a0da13d3
gech11 <getachewfikadu77@gmail.com>
2021-07-09 12:43:12 +0300

    initial commit

--- lib/comments.dart
+++ lib/comments.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:video/comment_card.dart';
 import 'package:http/http.dart' as http;
@@ -26,7 +28,7 @@ class _CommentState extends State<Comment> {
     return Container(
       child:Column(
         children: comments.map((comment){
-          return CommentCard(comment:comment); 
+          return CommentCard(comment); 
         }).toList(),
       )
     );
@@ -44,3 +46,24 @@ class _CommentState extends State<Comment> {
              hintText: 'comment here'
            ), 
           ),*/
+          DateTime now=DateTime.parse(time)
+          now.add(Duration(seconds:3));
+Navigator.pushReplacementNamed(context,'/video/show',arguments:{
+ videoId:this.videoId,
+});
+Map data=ModalRoute.of(context).settings.arguments;
+body:ListView.builder(
+  itemCount:videos.length,
+  itemBuilder:(context,index){
+    return Card(
+      child:ListTile(
+        onTap(){
+
+        },
+        leading:'leaing' ,
+        title: 'cool title',
+        trailing:'trailing'
+        )
+    )
+  }
+)
