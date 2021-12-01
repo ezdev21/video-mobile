dc7c29a671283693bde681f207cc1fed2c3ad01f
gech11 <getachewfikadu77@gmail.com>
2021-12-01 12:11:34 +0300

    initial commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:video/comments.dart';
+import 'package:video-mobile/comments.dart';
+import 'package:video-mobile/login_form.dart';
+
 class Drawer extends StatefulWidget {
   const Drawer({ Key? key }) : super(key: key);
 
@@ -23,14 +25,14 @@ class _DrawerState extends State<Drawer> {
            leading:Icon(Icons.logout),
            title:Text('logout'),
            onTap:(){
-
+            Navigator.push(context,MaterialPageRoute(builder:(context)=>LoginForm()))
            }
           ),
           ListTile(
            leading:Icon(Icons.logout),
            title:Text(''),
            onTap:(){
-
+            Navigator.push(context,MaterialPageRoute(builder:(context)=>))
            }
           )  
         ]
--- lib/services/login_form.dart
+++ lib/services/login_form.dart
@@ -31,7 +31,42 @@ class _LoinFormState extends State<LoinForm> {
       body:Form(
        key:_formKey,
        child:Column(
+         mainAxisAlignment:MainAxisAlignment.center,
+         children:[
+           TextFormField(
+            controller:_emailController,
+            validator:(val)=>val.isEmpty ? 'please enter a valid email' :null,
+            decoration:TextFieldDecoration(
 
+            )
+           ),
+           SizedBox(height:20),
+           TextFormField(
+            controller:_emailController,
+            validator:(val)=>val.isEmpty ? 'please enter a valid email' :null,
+            decoration:TextFieldDecoration(
+
+            )
+           ),
+           SizedBox(height:20),
+           TextButton(
+            Map creds={
+             'email' : _emailController.text,
+             'password' : _passwordController.text,
+             'device_name': 'android'
+            };
+            onPressed:(){
+             if(_formKey.currenState.validate()){
+
+             }
+            },
+            child:Text(
+              'Login',
+              style:TextStyle(fontSize:20,color:Colors.white)
+            ),
+            color:Colors.crimson[500]
+           ),
+         ]
        )
       )
     );
