19d3858f0c0ee2678e2bcea1a99ca7045eaa79f7
gech11 <getachewfikadu77@gmail.com>
2021-12-01 09:11:46 +0300

    initial commit

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,10 +1,48 @@
-import "package:video_mobile/material.dart";
+import "package:video-mobile/material.dart";
+import "package:video-mobile/models/video.dart";
+import "package:video-mobile/videos.dart";
+import "package:flutter_spinkit/flutter_spinkit.dart";
+import "package:http/http.dart";
+import "dart:convert";
 
-class Loading extends StatelessWidget {
-  const Loading({Key? key}) : super(key: key);
+class Loading extends StatefulWidget {
+  Loading({Key? key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  
+  List<Video> videos=[];
+  bool error=false;
+  void getVideos() async{
+   try{
+    Response response=await get('http://127.0.0.1/videos_injsonFormat');
+    videos=jsonDecode(response.body);
+    Naviator.push((context),MateriaPageRoute(builder:(context)=>Videos()),arguments:{videos:videos});
+   }
+   catch(e){
+     error=true;
+   }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getVideos();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor:Colors.crimson[500],
+      body:Center(
+        child:SpinKitFadingCube(
+          color:Colors.white,
+          size:80.0
+        )
+      )
+    );
   }
 }
