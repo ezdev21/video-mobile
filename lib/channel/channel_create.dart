778d73db5aba1e078ae5428e6f18ef76aa79cf6d
gech11 <getachewfikadu77@gmail.com>
2021-12-04 18:21:38 +0300

    initial commit

--- lib/channel/channel_create.dart
+++ lib/channel/channel_create.dart
@@ -8,13 +8,20 @@ class ChannelCreate extends StatefulWidget {
 }
 
 class _ChannelCreateState extends State<ChannelCreate> {
+  final _formKey=GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Container(
       child:Form(
+        key:_formKey,
         child:Column(
           TextFormField(
-            
+           decoration:textInputDecoration.copyWith(labelText:'channel name',hintText:'channnel name'),
+           validator:(val)=>val.length>2? 'channel name should be maximium of 3 characters' : null,
+           controller:_channelController.text,
+           onChanged:(){
+             
+           }
           )
         )
       )
